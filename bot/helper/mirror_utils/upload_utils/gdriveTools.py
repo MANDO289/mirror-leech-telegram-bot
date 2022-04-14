87022045f5ebbf36308ed0dbd269c201cc86f471
MANDO289 <75809523+MANDO289@users.noreply.github.com>
2022-04-14 02:20:48 +0200

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -169,7 +169,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by 𝕯𝕰𝖃𝕿𝕰𝕽',
+            'description': 'Uploaded by DEXTER',
             'mimeType': mime_type,
         }
         if parent_id is not None:
